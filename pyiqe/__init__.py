67eafd5139a19d168f79747fcc00eecddef35b9f
huy <huy@huyng.com>
2010-11-18 16:01:36 -0800

    fix indentation syntax error

--- pyiqe/__init__.py
+++ pyiqe/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
-__all__ = ['Api']
 
+__all__ = ['Api']
 import apis.api1_2
 STABLE_API_VERSION = "1.2"
 
@@ -13,7 +13,7 @@ def Api(*args, **kwargs):
     iqe = Api(API_KEY, API_SECRET, version="1.2")
 
     You can obtain api credentials at http://developer.iqengines.com/accounts/register/
-    
+
     For further information see the docstrings in the following class:
     
     pyiqe.apis.api1_2.Api
@@ -22,9 +22,7 @@ def Api(*args, **kwargs):
     
     version = kwargs.pop('version', STABLE_API_VERSION)
 
-    api_map  = {
-        "1.2" : apis.api1_2.Api,
-    }
+    api_map  = {"1.2" : apis.api1_2.Api}
     if not version in api_map:
         raise Exception("Invalid Api Version")
     ApiClass = api_map[version]
