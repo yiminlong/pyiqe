531bbb40edf552347e68665899908199e63aabf6
huy <huy@huyng.com>
2010-11-18 16:05:55 -0800

    added basic query test

--- tests.py
+++ tests.py
@@ -1,4 +1,30 @@
-
-def test_basic_import():
-    """Make sure we can atleast import everything"""
+    
+def test_basic_imports():
+    """ Make sure we can atleast import everything """
     from pyiqe import Api
+
+
+def _test_query_flow():
+    """ 
+    Tests whether querying works with the same interface. Warning, this will deplete your
+    purchased query count. Also it requires that you set IQE_KEY and IQE_SECRET in your
+    environment
+    """
+    import time
+    from pyiqe import Api
+    
+    iqe = Api(version="1.2")
+    response, qid = iqe.query('testdata/dole1.jpeg')
+    print("sending in query with qid %s" % qid)
+    assert response == {'data': {'error': 0}}, "Invalid Response while querying: \n%s " % response
+    
+    print("\nwaiting for results ...")
+    response = iqe.update()
+    print(response)
+    
+    print("\nretrieving results manually")
+    time.sleep(2)
+    response = iqe.result(qid)
+    print(response)
+
+    
