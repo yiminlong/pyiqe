98a3416668fb4e431228cfc4ea38e7ea4ded6d7e
huy <huy@huyng.com>
2010-12-03 12:36:09 -0800

    added apis, and api1_2 packages into the setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ setup(name='pyiqe',
       version='1.2',
       author='IQE',
       author_email='pierre@iqengines.com',
-      packages=['pyiqe'],)
+      packages=['pyiqe','pyiqe.apis', 'pyiqe.apis.api1_2'],)
