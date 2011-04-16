1f1e1fe3a4c0d8ddb626cf36f7d5a51b4e2de63c
huy <huy@huyng.com>
2011-04-15 17:12:21 -0700

    added test for multiple result feature

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
-import unittest    
 import time
+import unittest    
+from pprint import pprint 
+
 def test_basic_imports():
     """ Make sure we can atleast import everything """
     from pyiqe import Api
@@ -15,10 +17,8 @@ class TestApi(unittest.TestCase):
         
     def test_QueryExtra(self):
         """ Make sure we pass back extra argument is returned"""
-        import random
         import json
-        from pprint import pprint 
-        device_id = "pyiqe.test.%s" % random.random()
+        device_id = "pyiqe.test.%s" % time.time()
         response, qid = self.api.query('testdata/dole1.jpeg', extra={'a':1}, device_id=device_id)
         
         # update method
@@ -34,4 +34,14 @@ class TestApi(unittest.TestCase):
         pprint(result)
         assert "extra" in result and result['extra'] == json.dumps({'a':1}), "Extra Argument mismatch %s" % result
         
+    
+    def test_QueryMultipleResult(self):
+        # retrieve a result known to have multiple results
+        device_id = "pyiqe.test.%s" % time.time()
+        response, qid = self.api.query('testdata/multipleresult.jpeg', extra={'a':1}, device_id=device_id, multiple_results=True)
+        print qid
+        
+        result = self.api.update(device_id)
+        pprint(result)
+        assert False
         
